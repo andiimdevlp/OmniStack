5ee49cb5a676aabd09a92da335214424aab923c7
andiimdevlp <dev.cs.anderson@gmail.com>
2020-01-27 00:54:19 -0300

    Web Completo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
+const cors = require ('cors');
 const routes = require ('./routes');
 
 const app = express();
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://andiimhunter:4nd3rs0n@cluster0-shard-00-00-szuj6.mon
 
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
