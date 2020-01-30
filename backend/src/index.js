f7df5b7cc80065854b988a2fd62128c34ce24289
andiimdevlp <dev.cs.anderson@gmail.com>
2020-01-29 22:14:11 -0300

    Mobile finalizado

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
 const cors = require ('cors');
+const http = require ('http');
 const routes = require ('./routes');
+const { setupWebsocket } = require ('./websocket')
 
 const app = express();
+
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb://andiimhunter:4nd3rs0n@cluster0-shard-00-00-szuj6.mongodb.net:27017,cluster0-shard-00-01-szuj6.mongodb.net:27017,cluster0-shard-00-02-szuj6.mongodb.net:27017/week10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',{
     useNewUrlParser: true,
@@ -17,4 +23,4 @@ app.use(routes);
 
 
 
-app.listen(3333);
+server.listen(3333);
